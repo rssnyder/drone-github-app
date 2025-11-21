ee2c471be3b02399e80f4c851b6793c3346d1bc2
Hirsch Singhal <hpsin@github.com>
2025-11-21 11:47:36 -0800

    Add support for scoped tokens and client_id

--- plugin/util.go
+++ plugin/util.go
@@ -5,6 +5,7 @@
 package plugin
 
 import (
+	"bytes"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
@@ -63,14 +64,49 @@ func validateJWT(jwt string) (response AppResponse, err error) {
 }
 
 // installationToken returns a github pat for the given installation scope
-func installationToken(jwt, installation string) (response TokenResponse, err error) {
-	req, err := http.NewRequest("POST", fmt.Sprintf("https://api.github.com/app/installations/%s/access_tokens", installation), nil)
+// If repoData is provided, the token will be scoped to those repositories
+// If permissions is provided, the token will have those specific permissions
+func installationToken(jwt, installation string, repoData map[string]interface{}, permissions map[string]string) (response TokenResponse, err error) {
+	var reqBody *bytes.Buffer
+	
+	// Build request data with repositories and/or permissions if provided
+	// Expected JSON structure:
+	// {
+	//   "repositories": ["repo1", "repo2"] OR "repository_ids": [1001, 1002],
+	//   "permissions": {"contents": "read", "issues": "write"}
+	// }
+	reqData := make(map[string]interface{})
+	
+	if repoData != nil {
+		for key, value := range repoData {
+			reqData[key] = value
+		}
+	}
+	
+	if permissions != nil && len(permissions) > 0 {
+		reqData["permissions"] = permissions
+	}
+	
+	if len(reqData) > 0 {
+		jsonData, err := json.Marshal(reqData)
+		if err != nil {
+			return response, err
+		}
+		reqBody = bytes.NewBuffer(jsonData)
+	} else {
+		reqBody = bytes.NewBuffer([]byte{})
+	}
+	
+	req, err := http.NewRequest("POST", fmt.Sprintf("https://api.github.com/app/installations/%s/access_tokens", installation), reqBody)
 	if err != nil {
 		return
 	}
 
 	req.Header.Add("Accept", "application/vnd.github+json")
 	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", jwt))
+	if len(reqData) > 0 {
+		req.Header.Add("Content-Type", "application/json")
+	}
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
